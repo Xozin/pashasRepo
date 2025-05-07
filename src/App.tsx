2ee6781ecfe5cf29ba4eb7df2bbe39a989222a4d
Medve <medvedsvodkoi@gmail.com>
2025-05-07 22:23:40 +0300

    Я у мамы (и папы) программист(да я такой)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [formData, setFormData] = useState({name:'',});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        console.log(name, value)
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit  = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('Отправленные данные:', formData);
+        alert('Форма отправлена!');
+        // сброс (если нужно)
+        setFormData({ name: '', });
+    };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <h1>Наше Гавно</h1>
+          <form className="form" onSubmit={handleSubmit}>
+          <div className="card">
+              <label htmlFor="name" className="form-label">Введи Артикул</label>
+              <input name="name" type="text" className="form-input" placeholder="Артикул" value={formData.name} onChange={handleChange}/>
+              <button type="submit" className="form-button">Поиск</button>
+          </div>
+          </form>
+          <div className="create-line"></div>
+      </>
   )
 }
 
