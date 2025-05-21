aace083d4ef51e4ad72bb7a3c837402f65fa2e6f
Medve <medvedsvodkoi@gmail.com>
2025-05-21 14:57:51 +0300

    added arrows and Enter

--- src/App.tsx
+++ src/App.tsx
@@ -14,46 +14,48 @@ const data: IData = untypedData;
 
 // Компонент виртуализированного списка
 const renderRow = ({ data, index, style }: { data: any; index: number; style: React.CSSProperties }) => {
-  return (
-    <div
-      style={{
-        ...style,
-        border: 'none',
-        display: 'block',
-        textAlign: 'left',
-        lineHeight: '34px',
-      }}
-      className={'myli'}
-    >
-      {data[index]}
-    </div>
-  );
+    return (
+        <div
+            style={{
+                ...style,
+                border: 'none',
+                display: 'block',
+                textAlign: 'left',
+                lineHeight: '34px',
+            }}
+            className={'myli'}
+            role="option"
+            aria-selected={false}
+        >
+            {data[index]}
+        </div>
+    );
 };
 
 const VirtualizedListbox = React.forwardRef(function VirtualizedListbox(
-  props: React.HTMLAttributes<HTMLElement>,
-  ref: React.Ref<HTMLDivElement>
+    props: React.HTMLAttributes<HTMLElement>,
+    ref: React.Ref<HTMLDivElement>
 ) {
-  const { children, ...other } = props;
+    const { children, ...other } = props;
 
-  const itemData = React.Children.toArray(children);
-  const itemCount = itemData.length;
-  const itemSize = 32; // Высота элемента списка
+    const itemData = React.Children.toArray(children);
+    const itemCount = itemData.length;
+    const itemSize = 32; // Высота элемента списка
 
-  return (
-    <div ref={ref} {...other}>
-      <VariableSizeList
-        height={Math.min(8 * itemSize, 300)} // Максимальная высота списка
-        width="100%"
-        itemSize={() => itemSize}
-        itemCount={itemCount}
-        itemData={itemData}
-        overscanCount={5} // Сколько элементов загружать за пределами видимой области
-      >
-        {renderRow}
-      </VariableSizeList>
-    </div>
-  );
+    return (
+        <div ref={ref} {...other} role="listbox">
+            <VariableSizeList
+                height={Math.min(12 * itemSize, 300)} // Максимальная высота списка
+                width="100%"
+                itemSize={() => itemSize}
+                itemCount={itemCount}
+                itemData={itemData}
+                overscanCount={5} // Сколько элементов загружать за пределами видимой области
+            >
+                {renderRow}
+            </VariableSizeList>
+        </div>
+    );
 });
 
 function App() {
@@ -65,40 +67,37 @@ function App() {
         e.preventDefault();
     };
 
-
-  return (
-    <>
-      <h1>Ведро артикулов</h1>
-      <form className="form" onSubmit={handleSubmit}>
-      <div className="card">
-        <Autocomplete
-          disablePortal
-          style={{width:'700px'}}
-          value={value}
-          onChange={(_event: any, newValue: string | null) => {
-              setValue(newValue);
-          }}
-          inputValue={inputValue}
-          onInputChange={(_event, newInputValue) => {
-              setInputValue(newInputValue);
-          }}
-          slots={{
-            listbox: VirtualizedListbox, // Используем кастомный виртуализированный список
-          }}
-          id="controllable-states-demo"        options={keys}
-                  sx={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Артикул" />}
-              />
-              {/*<Button type="submit" className="form-button">Поиск</Button>*/}
-          </div>
-          </form>
-          <div className="create-line"></div>
-      {value && <div className="cardBoard">
-          <CardList article={value} price={data[value]} />
-
-      </div> }
-      </>
-  )
+    return (
+        <>
+            <h1>Ведро артикулов</h1>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="card">
+                    <Autocomplete
+                        disablePortal
+                        style={{width:'700px'}}
+                        value={value}
+                        onChange={(_event: any, newValue: string | null) => {
+                            setValue(newValue);
+                        }}
+                        inputValue={inputValue}
+                        onInputChange={(_event, newInputValue) => {
+                            setInputValue(newInputValue);
+                        }}
+                        id="controllable-states-demo"
+                        options={keys}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Артикул" />}
+                        //без виртуализации(этой строчки) вообще все идеально работает, понакрутил свистелок, блин )))
+                        ListboxComponent={VirtualizedListbox as React.ComponentType<React.HTMLAttributes<HTMLElement>>}
+                    />
+                </div>
+            </form>
+            <div className="create-line"></div>
+            {value && <div className="cardBoard">
+                <CardList article={value} price={data[value]} />
+            </div> }
+        </>
+    )
 }
 
 export default App
